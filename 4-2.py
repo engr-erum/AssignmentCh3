e8a43d3e59cd295d40422c45f91619a4eb005749
engr-erum <erumhannan32@yahoo.com>
2017-04-12 17:40:12 +0500

    added 4-1 and 4-2 codes

---
+++ 4-2.py
@@ -0,0 +1,14 @@
+#4-2 Animals page no 60
+animals = ["dog", "cat","rabbit"];
+
+# individual names of animals from list 
+for animalsName in animals:
+	print(animalsName);
+print("");
+
+# names with additional text 
+for animalsName in animals:
+	print("A " + animalsName + " would make a great pet.");
+print("");	
+
+print("Any of these animals would make a great pet!");
