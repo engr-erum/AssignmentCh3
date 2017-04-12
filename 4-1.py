b5af7192438e7377b28c7be8f56e9a993128b7ea
engr-erum <erumhannan32@yahoo.com>
2017-04-12 18:03:17 +0500

    Add files via upload

---
+++ 4-1.py
@@ -0,0 +1,14 @@
+#4-1 pizzas page no 60
+pizza_list  = ["peproni","fajita","tikka"];
+# simple print name
+for pizza_name in pizza_list:
+	print(pizza_name);
+	
+# print name with additional text
+for pizza_name in pizza_list:
+	print("I like " + pizza_name + " Pizza");	
+	
+print("How much you like pizza");
+print("You like peproni pizza");
+print("You like chicken tikka pizza");
+print("I really love pizza");
