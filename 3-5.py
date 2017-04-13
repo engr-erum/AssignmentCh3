0e8a6ca75107d8fa4b39d12fc9a5954cd9738f00
engr-erum <erumhannan32@yahoo.com>
2017-04-13 17:44:20 +0500

    removed extra print("")

--- 3-5.py
+++ 3-5.py
@@ -5,18 +5,17 @@ print("------------------------");
 for invitePerson in inviteList:
 	print("Hello " + invitePerson + "," + " I would like to invite you for today's dinner.");
 personCantMake = "Sara";
-print("");
-print(personCantMake + " cannot make dinner.");
+
+print("\n" + personCantMake + " cannot make dinner.");
 # trying to get index for not coming person
 indexCantMake = inviteList.index(personCantMake);
 # alter list index name
 inviteList[indexCantMake] = "Ahmed";
-print("");
-print("Modified List");
+
+print("\n Modified List");
 print("-------------");
 print(inviteList);
-print("");
-print("Second Set of Invitation");
+print("\n Second Set of Invitation");
 print("------------------------");
 for invitePerson in inviteList:
 	print("Hello " + invitePerson + "," + " I would like to invite you for today's dinner.");
