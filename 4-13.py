e51fc381ad47e3551cb54db848a6ed5ad169eb68
engr-erum <erumhannan32@yahoo.com>
2017-04-13 17:30:04 +0500

    uploaded 4-13.py

---
+++ 4-13.py
@@ -0,0 +1,9 @@
+dinner_list = ("chicken Tikka","Biryani","Zinger Burger","Sandwich","Ice cream");
+for items in dinner_list:
+	print(items);
+
+# dinner_list[0] = "Roti";
+dinner_list = ("Roti","Karahi","Zinger Burger","Sandwich","Ice cream");
+
+for items in dinner_list:
+	print(items);
